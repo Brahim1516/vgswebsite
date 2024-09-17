bd1a9373ee1538845511337f22ce5372de4289e7
Brahim1516 <152946108+Brahim1516@users.noreply.github.com>
2024-09-17 18:00:00 +0100

    app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, Flask!'
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    # Set the host and port to make the app accessible on a specific IP and port
-    app.run(host='159.69.244.232', port=8080, debug=True)
+    # Run Flask on a specific IP and port
+    app.run(host='159.69.244.232', port=80, debug=True)
